fe1079d44a842a9f5f6b0a4d1ca06cb45e01b510
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2023-10-17 16:06:18 +0530

    Create class_obj.cpp

---
+++ class_obj.cpp
@@ -0,0 +1,32 @@
+
+// C++ program to implement 
+// the above approach
+#include<iostream>
+using namespace std;
+ 
+class Employee
+{
+  int id;
+  char name[30];
+  public:
+  void getdata();//Declaration of function
+  void putdata();//Declaration of function
+};
+void Employee::getdata(){//Defining of function
+  cout<<"Enter Id : ";
+  cin>>id;
+  cout<<"Enter Name : ";
+  cin>>name;
+}
+void Employee::putdata(){//Defining of function
+  cout<<id<<" ";
+  cout<<name<<" ";
+  cout<<endl;
+}
+int main(){
+  Employee emp; //One member 
+  emp.getdata();//Accessing the function
+  emp.putdata();//Accessing the function
+  return 0;
+ 
+}
