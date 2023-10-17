50731340a52b4d7b55b0f9af2511241665080050
ugsakshii20 <138098129+ugsakshii20@users.noreply.github.com>
2023-10-17 16:10:06 +0530

    Create usingarray.cpp

---
+++ usingarray.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Book
+{
+
+    public:
+        char name[50];
+        int cost;
+
+        void getdetails();
+
+        void putdetails();
+};
+
+void Book::getdetails()
+{
+
+    cout << "Enter Name : ";
+    cin >> name;
+    cout << "Enter Cost : ";
+    cin >> cost;
+
+}
+
+void Book::putdetails()
+{
+
+    cout << name << " ";
+    cout << cost << " ";
+    cout << endl;
+
+}
+
+int main()
+{
+
+    Book bk[100];
+    int n, i;
+
+    cout << "Enter Number of Books - ";
+    cin >> n;
+
+    for(i = 0; i < n; i++){
+        bk[i].getdetails();
+    }
+
+    cout << "Details of Books - " << endl;
+
+    for(i = 0; i < n; i++){
+        bk[i].putdetails();
+    }
+
+    return 0;
+
+}
